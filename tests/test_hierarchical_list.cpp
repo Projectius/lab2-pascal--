bf0d931227ed993a9b7d65d57a7869d2d3155e0b
VALancaster <valentin.kutergin@gmail.com>
2025-05-05 22:52:39 +0300

    add tests

---
+++ tests/test_hierarchical_list.cpp
@@ -0,0 +1,12 @@
+#include "gtest.h"
+#include "hierarchical_list.h"
+
+TEST(HierarchicalListTest, can_add_next)
+{
+
+}
+
+TEST(HierarchicalListTest, can_add_child)
+{
+
+}
---
+++ tests/test_parser.cpp
@@ -0,0 +1,12 @@
+#include "gtest.h"
+#include "parser.h"
+
+TEST(ParserTest, can_check_for_errors)
+{
+	
+}
+
+TEST(ParserTest, can_build_hlist)
+{
+
+}
